f0bc25f67e48455440475b1915e0be76b7807b84
WiseBoiii <aleksashin.1001@gmail.com>
2020-09-10 22:29:34 +0300

    download book covers

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import lxml
 import os
 from pathvalidate import sanitize_filename
 from pathvalidate import sanitize_filepath
+import urllib
 
 
 def download_txt(download_url, title, folder='books/'):
@@ -16,6 +17,17 @@ def download_txt(download_url, title, folder='books/'):
         my_file.write(response.text)
 
     return filepath
+
+def download_image(image_url, picture, book_id, folder='previews/'):
+    response = requests.get(image_url)
+    if picture == '/images/nopic.gif':
+        file_name = 'nopic'+ str(book_id) + '.gif'
+        pass
+    else:
+        file_name = str(book_id) + '.jpg'
+    filepath = os.path.join(folder, file_name)
+    with open(filepath, "wb") as my_file:
+        my_file.write(response.content)
 
 
 url_pattern = 'http://tululu.org/'
@@ -32,6 +44,10 @@ for book_id in range(1, 10):
     title_and_author = title_tag.text
     title_and_author = title_and_author.split('::')
     title = title_and_author[0].rstrip()
+    picture = soup.find('div', class_='bookimage').find('img')['src']
+    print(picture)
+    image_url = urllib.parse.urljoin(url_pattern, picture)
+    print(image_url)
     download_url = f"{url_pattern}/txt.php?id={book_id}"
     download_txt(download_url, title, 'books/')
-
+    download_image(image_url, picture, book_id)
