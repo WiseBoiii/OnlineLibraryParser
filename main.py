63d8999ec0bd36332ffad06fe5c753862aadc2b0
WiseBoiii <aleksashin.1001@gmail.com>
2020-10-06 20:47:31 +0300

    download comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import requests
 from bs4 import BeautifulSoup
 import lxml
 import os
@@ -30,13 +29,22 @@ def download_image(image_url, picture, book_id, folder='previews/'):
         my_file.write(response.content)
 
 
-url_pattern = 'http://tululu.org/'
-for book_id in range(1, 10):
+url_pattern = 'https://tululu.org/'
+for book_id in range(1,11):
     url = f"{url_pattern}b{book_id}/"
+    download_url = ''
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 301:
-        continue
     soup = BeautifulSoup(response.text, 'lxml')
+    try:
+        download_link_tag = soup.find('table', class_='d_book').find_all('a')
+    except AttributeError:
+        continue
+    for link in download_link_tag:
+        if 'txt' in link['href']:
+            download_link = link['href']
+            download_url = f"{url_pattern}{download_link}"
+    if response.status_code == 302:
+        continue
     try:
         title_tag = soup.find('div', id='content').find('h1')
     except AttributeError:
@@ -45,9 +53,16 @@ for book_id in range(1, 10):
     title_and_author = title_and_author.split('::')
     title = title_and_author[0].rstrip()
     picture = soup.find('div', class_='bookimage').find('img')['src']
-    print(picture)
-    image_url = urllib.parse.urljoin(url_pattern, picture)
-    print(image_url)
-    download_url = f"{url_pattern}/txt.php?id={book_id}"
-    download_txt(download_url, title, 'books/')
-    download_image(image_url, picture, book_id)
+    comment_section_tag = soup.find('div', id='content').find_all('div', class_='texts')
+    print(comment_section_tag)
+    #comment_tag = comment_section_tag.find_all('span', class_='black')
+    #print(comment_tag)
+    #except AttributeError:
+        #continue
+    #comment = comment_tag.text
+    #print(title)
+    #print(comment)
+    #image_url = urllib.parse.urljoin(url_pattern, picture)
+    #if download_url:
+        #download_txt(download_url, title, 'books/')
+    #download_image(image_url, picture, book_id)
